ae79a1d0f174f6918027eaca8806f162522d3dc4
mozammilanwar <mozammilanwar66@gmail.com>
2023-05-02 14:53:59 +0530

    done

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -2,37 +2,39 @@ package com.driver;
 
 public class Order {
 
-    private final String id;
+    private String id;
     private int deliveryTime;
+
+
 
     public Order(String id, String deliveryTime) {
 
+        this.id = id;
+////         char[] arr = new char[2];
+////         arr[0] = deliveryTime.charAt(0);
+//         String s1 = String.valueOf(deliveryTime.charAt(0) + deliveryTime.charAt(1));
+//         String s2 = String.valueOf(deliveryTime.charAt(3) + deliveryTime.charAt(4));
+//         int hh = Integer.valueOf(s1);
+//         int mm = Integer.valueOf(s2);
+//
+//         int time = (hh*60)+mm;
+        String arr[] = deliveryTime.split(":");
+        int hh = Integer.parseInt(arr[0]);
+        int mm = Integer.parseInt(arr[1]);
+        int time = (hh*60)+mm;
+
+        this.deliveryTime = time;
+
+
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
-        this.id=id;
-        this.deliveryTime=convrt(deliveryTime);
     }
-    private int convrt(String dT){
-        String h="";
-        String m="";
-        boolean met=false;
-        for(char c:dT){
-            if(c==':'){
-                met=true;
-                continue;
-            }
-            if(met){
-                m+=c;
-            }
-            else{
-                h+=c;
-            }
-        }
-        return (Integer.parseInt(h))*60+(Integer.parseInt(m));
-    }
+
     public String getId() {
         return id;
     }
 
     public int getDeliveryTime() {return deliveryTime;}
+
+
 }
