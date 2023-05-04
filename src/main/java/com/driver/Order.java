114f85c8b2e91da1a01eb64ccbf142602e91c2a3
mozammilanwar <mozammilanwar66@gmail.com>
2023-05-05 00:40:12 +0530

    done

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,33 +1,25 @@
 package com.driver;
 
+import io.swagger.models.auth.In;
+
 public class Order {
 
     private String id;
     private int deliveryTime;
 
-
-
     public Order(String id, String deliveryTime) {
-
-        this.id = id;
-////         char[] arr = new char[2];
-////         arr[0] = deliveryTime.charAt(0);
-//         String s1 = String.valueOf(deliveryTime.charAt(0) + deliveryTime.charAt(1));
-//         String s2 = String.valueOf(deliveryTime.charAt(3) + deliveryTime.charAt(4));
-//         int hh = Integer.valueOf(s1);
-//         int mm = Integer.valueOf(s2);
-//
-//         int time = (hh*60)+mm;
-        String arr[] = deliveryTime.split(":");
-        int hh = Integer.parseInt(arr[0]);
-        int mm = Integer.parseInt(arr[1]);
-        int time = (hh*60)+mm;
-
-        this.deliveryTime = time;
-
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+
+        this.id=id;
+
+        String arr[]=deliveryTime.split(":");//12:45
+        int hr=Integer.parseInt(arr[0]);
+        int min=Integer.parseInt(arr[1]);
+
+        this.deliveryTime=(hr*60+min);
+
     }
 
     public String getId() {
@@ -35,6 +27,4 @@ public class Order {
     }
 
     public int getDeliveryTime() {return deliveryTime;}
-
-
 }
